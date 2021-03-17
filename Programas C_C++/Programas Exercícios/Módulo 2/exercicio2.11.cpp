bc04c2254d5f3959a6e7f2ede278395fa093de06
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2021-03-17 20:54:52 +0000

    novos exercicios

---
+++ Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.11.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    double variavel2;
+
+    return 0;
+}
--- Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.9.cpp
+++ Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.9.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-    
+    double variavel;
 
     return 0;
 }
