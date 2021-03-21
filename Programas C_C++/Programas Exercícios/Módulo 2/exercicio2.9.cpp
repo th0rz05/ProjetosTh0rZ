106bb589c0b5bde936bf2c6fb776eea5ee68a7d2
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2021-03-21 21:46:43 +0000

    exercicio 2.9

--- Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.9.cpp
+++ Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.9.cpp
@@ -1,3 +1,4 @@
+/* Determines whether or not a number is a palindrome */
 
 #include <iostream>
 
@@ -5,8 +6,45 @@ using namespace std;
 
 int main(){
 
-    double variavel2;
+    //initialize
+    int digit1,digit2,number,length,temp_len,num_temp;
+    bool palindrome;
+
+    palindrome = true;
+
+    cout << "Input number: ";
+    cin >> number;
+
+    num_temp = number;
+
+    //calculate length
+    while (num_temp>0){
+        num_temp = num_temp/10;
+        length+=1;
+    }
+
+    temp_len=length;
+
+    //see if is palindrome
+    for(int i=1;i<=(length/2);i++){
+        digit1= number % int(pow(10,1));
+        digit2= number / pow(10,temp_len-1);
+        if(digit1!=digit2){
+            palindrome = false;
+            break;
+        }
+        number=number-digit2*pow(10,temp_len-1);
+        number=number/10;
+        temp_len = temp_len-2; 
+    }
     
+    //output
+    if (palindrome){
+        cout << "It's a palindrome";
+    }
+    else {
+        cout << "It isn't a palindrome";
+    }
 
 
     return 0;
