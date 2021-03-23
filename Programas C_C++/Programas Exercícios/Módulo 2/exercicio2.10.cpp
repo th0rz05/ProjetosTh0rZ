dfaa27dcf46cdae10ae4388c51d27f2eceeb8f4a
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2021-03-23 01:24:04 +0000

    exercicio2.10

--- Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.10.cpp
+++ Programas C_C++/Programas Exercícios/Módulo 2/exercicio2.10.cpp
@@ -1,3 +1,4 @@
+/* Reads an integer and breaks it down into prime factors */
 
 #include <iostream>
 
@@ -5,9 +6,50 @@ using namespace std;
 
 int main(){
 
-    double variavel2;
+    //input
+    int number;
+    bool prime=false;
+
+    cout << "Input number: ";
+    cin >> number;
+
+    cout << number<< " prime factors are ";
+
+    //see if is prime
+    for (int i=2;i<=sqrt(number);i++){
+        if(number%i==0){
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime==true){
+        cout << number ;
+        return 0;
+    }
     
+    //factorizes in 2
+    while (number % 2 == 0)  
+    {  
+        cout << 2 << " ";  
+        number = number/2;  
+    }  
+    
+    //factorizes in the rest of primes
+    for (int i = 3; i <= 100; i = i + 2)  
+    {  
+        
+        while (number % i == 0)  
+        {  
+            cout << i << " ";  
+            number = number/i;  
+        }  
+    }
 
-
+    //last number if prime show
+    if (number>1){
+        cout << number << endl;
+    }
+    
     return 0;
 }
