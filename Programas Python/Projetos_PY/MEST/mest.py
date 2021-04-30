02fc5a58698da089763cb3c9ddc9dce8fb98dd71
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2021-04-30 23:29:20 +0100

    mest.py

--- Programas Python/Projetos_PY/MEST/mest.py
+++ Programas Python/Projetos_PY/MEST/mest.py
@@ -1,16 +1,23 @@
 import math
 
 lista = [-352,-297,-242,-187,-132,-77,-22,33,88,33,-22,-77,-132]
-prob =[]
-
-for n in range(0,12):
-    prob.append((math.factorial(12)/(math.factorial(n)*math.factorial(12-n)))*(0.6**n)*(0.4**(12-n)))
-
+probabilidades =[]
+media = 0
 variancia=0
 
-for i in range(0,11):
-    variancia += (lista[i]-7.64)**2*prob[i]
-    
-desvio = math.sqrt(variancia)
+for n in range(0,12):
+    combinacoes = math.factorial(12)/(math.factorial(n)*math.factorial(12-n))
+    sucesso = 0.6**n
+    insucesso = 0.4**(12-n)
+    probabilidades.append(combinacoes*sucesso*insucesso)
 
+for s in range(0,12):
+    media += lista[s]*probabilidades[s]
+
+for i in range(0,11):
+    variancia += (lista[i]-media)**2*probabilidades[i]
+    
+desvio = variancia**0.5
+
+print(media)
 print(desvio)
